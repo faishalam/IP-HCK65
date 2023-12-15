a560046ccad5f5d33fcbd9247d3aa8a41519ca0f
faishalam <faishalabdulmajid@gmail.com>
2023-12-15 09:11:16 +0700

    deploy

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken");
-// const SECRET_KEY = process.env.SECRET_KEY
+const SECRET_KEY = process.env.SECRET_KEY
 
 const signToken = (payload) => {
-    return jwt.sign(payload, "secret")
+    return jwt.sign(payload, SECRET_KEY)
 }
 
 const verifyToken = (token) => {
-    return jwt.verify(token, "secret")
+    return jwt.verify(token, SECRET_KEY)
 }
 
 module.exports = {
